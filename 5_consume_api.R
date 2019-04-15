3dba336fbfd1f093432ef8fc3377554afaa3e5e1
andrie <andrie@rstudio.com>
2019-04-15 14:13:26 +0000

    Move to root

---
+++ 5_consume_api.R
@@ -0,0 +1,20 @@
+library(httr)
+
+predict_peptide <- function(peptide){
+  solo_url = "https://colorado.rstudio.com/rsc/content/2337/"
+  if (substring(solo_url, nchar(solo_url)) != "/") solo_url <- paste0(solo_url, "/")
+  api_url <- paste0(solo_url, "/predict")
+
+  r <- GET(api_url, query = list(peptide = peptide), encode = "json", content_type_json())
+  if (httr::http_error(r)){
+    cat(http_status(r))
+    stop(http_status(r))
+  }
+  r %>%
+    content() %>%
+    as.data.frame()
+}
+
+
+predict_peptide(peptide = "LLTDAQRIV")
+predict_peptide(c("LLTDAQRIV", "LMAFYLYEV", "VMSPITLPT", "SLHLTNCFV", "RQFTCMIAV"))
