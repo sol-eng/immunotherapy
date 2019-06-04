c02ec1c09867c885ee4be1212640e9c443ee8ed6
Andrie de Vries <apdevries@gmail.com>
2019-06-04 16:02:19 +0000

    Handle different types of input

--- 5_consume_api.R
+++ 5_consume_api.R
@@ -1,7 +1,11 @@
 library(httr)
+library(purrr)
 
 predict_peptide <- function(peptide, solo_url = config::get("solo_url_plumber")){
   if (substring(solo_url, nchar(solo_url)) != "/") solo_url <- paste0(solo_url, "/")
+
+  if(length(peptide > 1)) peptide <- paste(peptide, collapse = ",")
+
   api_url <- paste0(solo_url, "/predict")
 
   r <- GET(api_url, query = list(peptide = peptide), encode = "json", content_type_json())
@@ -9,11 +13,17 @@ predict_peptide <- function(peptide, solo_url = config::get("solo_url_plumber"))
     cat(http_status(r))
     stop(http_status(r))
   }
-  r %>%
-    content() %>%
-    as.data.frame()
+
+  rc <- content(r)
+
+  map_dfr(
+    rc,
+    ~as.data.frame(., stringsAsFactors = FALSE)
+  )
 }
 
 
 predict_peptide(peptide = "LLTDAQRIV")
+predict_peptide("LLTDAQRIV, LMAFYLYEV, VMSPITLPT, SLHLTNCFV, RQFTCMIAV")
 predict_peptide(c("LLTDAQRIV", "LMAFYLYEV", "VMSPITLPT", "SLHLTNCFV", "RQFTCMIAV"))
+
