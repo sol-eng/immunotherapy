488343121805a583e84ffda9a7c59aedc21362a8
Alex Gold <alex.gold@rstudio.com>
2020-08-26 21:23:46 +0000

    err if content URL not set

--- 4_consume_api.R
+++ 4_consume_api.R
@@ -11,16 +11,23 @@ predict_peptide <- function(peptide,
     peptide <- paste(peptide, collapse = ",")
   }
 
-  url %>%
-    httr::GET(
-      query = list(peptide = peptide),
-      encode = "json",
-      httr::content_type_json(),
-      add_headers(Authorization = paste0("Key ", con$rsc_api_key))
-    ) %>%
-    httr::stop_for_status() %>%
-    httr::content() %>%
-    map_dfr(tibble::as_tibble)
+  tryCatch({
+    url %>%
+      httr::GET(
+        query = list(peptide = peptide),
+        encode = "json",
+        httr::content_type_json(),
+        add_headers(Authorization = paste0("Key ", con$rsc_api_key))
+      ) %>%
+      httr::stop_for_status() %>%
+      httr::content() %>%
+      map_dfr(tibble::as_tibble)
+  }, error = function(e) {
+    if (grepl("404", as.character(e))) {
+      stop("Content not found, did you set the content URL to match your config (immuno_api)?")
+    }
+    e
+  })
 }
 
 # Test it
